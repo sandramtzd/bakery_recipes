9c247b9130e22a44a2b1b71a1767ad7835645e6a
Sandra <sandramtzd@hotmail.com>
2023-09-02 13:36:37 +0100

    tracking revenue and mapped cakes

--- src/containers/CakeContainer.js
+++ src/containers/CakeContainer.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import Cake from "../components/Cake";
 
-const CakesContainer = () => {
+const CakeContainer = () => {
 
-    const [cakes, setCakes] = useState(
+    const [cakes, setCakes] = useState([
         {
             cakeName: "Victoria Sponge",
             ingredients: [
@@ -51,9 +51,8 @@ const CakesContainer = () => {
 
 
 
-    );
+    ]); // stablishing cakes's array by adding square brackets
 
-    
 
     //EXTENSION -TRACKING REVENUE
 
@@ -63,19 +62,30 @@ const CakesContainer = () => {
         setRevenue(revenue + saleValue);
     }
 
+    const rating = cakes.victoraSponge.rating + cakes.teaLoaf.rating + cakes.carrotCake.rating 
+
+    const mappedCakes = cakes.map((cake, id) => {
+        return <Cake
+                    cake = {cake}
+                    updateRevenue = {updateRevenue}
+                />
+    })
 
     return(
         <>
             <div className= "container">
-                <Cake cake={victoraSponge} updateRevenue={updateRevenue}/>
-                <Cake cake={teaLoaf} updateRevenue={updateRevenue}/>
-                <Cake cake={carrotCake} updateRevenue={updateRevenue}/>
+                {mappedCakes}
             </div>
-            <p>Average Rating: {(victoraSponge.rating)}</p>
+            <p id = "rating">Average Rating: {rating}</p>
             <p id = "price">Total Sales: £{revenue}</p>
         
         </>
+       
+    );
+}
+export default CakeContainer;
 
+// FIRST ATTEMPT
         // <>
         //     <h2>Cake Recipes</h2>
         //     <RecipeComponent
@@ -84,26 +94,3 @@ const CakesContainer = () => {
         //         price={5}
         //         rating={5}
         //         onButtonClick={setTotalSales}
-        //     />
-        //     <RecipeComponent
-        //         cakeName={"Tea Loaf"}
-        //         ingredients={["eggs, oil, dried fruit, sugar, self-raising flour, strong tea"]}
-        //         price={2}
-        //         rating={5}
-        //         onButtonClick={setTotalSales}
-        //     />
-        //     <RecipeComponent
-        //         cakeName={"Carrot Cake"}
-        //         ingredients={["carrots, walnuts, oil, cream, cheese, flour, sugar"]}
-        //         price={8}
-        //         rating={5}
-        //         onButtonClick={setTotalSales}
-        //     />
-        //     <p>Average Rating: X</p>
-        //     <p>Total Sales: {totalSales} £</p>
-
-        
-        // </>
-    );
-}
-export default CakeContainer;
